3a912ea654fcd4167c6dc0573acc682aabf12d99
Justin Schaare <jschaare16@gmail.com>
2021-06-19 01:07:18 -0400

    add dynamic packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, "README.md"), encoding="utf-8") as f:
@@ -15,7 +15,7 @@ setup(
     license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=["osrs"],
+    packages=find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
